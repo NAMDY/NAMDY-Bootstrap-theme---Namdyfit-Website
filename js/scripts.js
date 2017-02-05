5886531e80e584b9733ec256e4892d86b002d2bf
Nnamdi Obishai <namdyobishai@gmail.com>
2017-02-06 05:55:17 +0600

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('.dropdown-toggle').dropdown();
+});
+
+jQuery(function($) {
+    $('form[data-async]').live('submit', function(event) {
+        var $form = $(this);
+        var $target = $($form.attr('data-target'));
+
+        $.ajax({
+            type: $form.attr('method'),
+            url: $form.attr('action'),
+            data: $form.serialize(),
+
+            success: function(data, status) {
+                $target.html(data);
+            }
+        });
+
+        event.preventDefault();
+    });
+});
